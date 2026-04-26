51c318606d2a4c097ca64a13d88dc2f38b004851
kazzutora <diabloroale9@gmail.com>
2026-04-27 00:37:26 +0200

    create models rooms to DB, create migrations and push upgrade head(alembic)

--- src/models/hotels.py
+++ src/models/hotels.py
@@ -1,5 +1,5 @@
 from sqlalchemy.testing.schema import mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from src.database import Base
 from sqlalchemy.orm import Mapped
 
@@ -10,3 +10,13 @@ class HotelsOrm(Base):
     id: Mapped[int]= mapped_column(primary_key=True)
     title: Mapped[str]= mapped_column(String(length=100))
     location: Mapped[str]= mapped_column(String(length=100))
+
+class RoomOrm(Base):  # Окремий клас для кімнат
+    __tablename__ = 'rooms'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    hotel_id: Mapped[int] = mapped_column(ForeignKey('hotels.id'))
+    title: Mapped[str] = mapped_column(String(length=100))
+    description: Mapped[str | None]
+    price: Mapped[int]
+    quantity: Mapped[int]
