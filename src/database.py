e58944c8875e4743f5f5789bcb7c02f1b876185c
kazzutora <diabloroale9@gmail.com>
2026-04-27 00:09:38 +0200

    create backend project its education. FASTAPI SQLALCHEMY , ALEMBIC ... create models and push migrations to database and building structure project 'booking Hotels' on Python

---
+++ src/database.py
@@ -0,0 +1,14 @@
+import asyncio
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+from sqlalchemy import text
+from src.config import settings
+from sqlalchemy.orm import DeclarativeBase
+
+engine = create_async_engine(settings.DB_URL)
+
+async_session_maker = async_sessionmaker(bind = engine , expire_on_commit = False)
+
+session = async_session_maker()
+
+class Base(DeclarativeBase):
+    pass
---
+++ src/models/hotels.py
@@ -0,0 +1,12 @@
+from sqlalchemy.testing.schema import mapped_column
+from sqlalchemy import String
+from src.database import Base
+from sqlalchemy.orm import Mapped
+
+class HotelsOrm(Base):
+    __tablename__ = 'hotels'
+
+
+    id: Mapped[int]= mapped_column(primary_key=True)
+    title: Mapped[str]= mapped_column(String(length=100))
+    location: Mapped[str]= mapped_column(String(length=100))
